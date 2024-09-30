f6e4d962e5395ba9039e8344971079e6bae4f0c9
Patrick <patrickprada2002@gmail.com>
2024-09-30 14:08:08 -0500

    Se creo la lista de criptomonedas se creo axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
-
+import { useEffect, useState } from "react"
+import axios from "axios"
 
 function App() {
+  
+  const API_URL=import.meta.env.VITE_API_URL
+  const [criptos, setCriptos]=useState()
+
+  useEffect(()=>{
+    fetch(`${API_URL}assets`)
+      .then((resp)=>resp.json())
+      .then((data)=>{
+        setCriptos(data.data)
+      })
+      .catch(()=>{
+        console.error("La petición falló")
+      })
+  },[])
+
+  if(!criptos) return <span>Cargando...</span>
 
   return (
-    <h1>Hola mundo</h1>
+    <>  
+      <h1>Lista de Criptomonedas</h1>
+        <ol>
+          {criptos.map(({name, priceUsd})=>(
+            <li>Nombre:{name} Precio: {priceUsd}</li>
+          ))}
+        </ol>
+    </>
   )
 }
 
